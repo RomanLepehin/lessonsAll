efcf548b0ff8bbff2629ebd7d092c48fed7fcff6
RomanLepehin <rlepehin@mail.ru>
2019-01-07 14:52:09 +0600

    AutoSlider
    left and right button and autoSlider

--- 1.js
+++ 1.js
@@ -2,6 +2,7 @@ document.getElementById('btn_l').onclick = sliderLeft;
 document.getElementById('btn_r').onclick = sliderRight;
 
 var left = 0;
+var leftAuto = 0;
 function sliderLeft() {
     var p;
     p = document.getElementById('polosa');
@@ -21,3 +22,18 @@ function sliderRight() {
     }
     p.style.left = left + 'px';
 }
+
+function autoSlider() {
+	setTimeout(function() {
+		var p2;
+    	p2 = document.getElementById('polosa2');
+		leftAuto = leftAuto - 256;
+    	if (leftAuto < -1280) {
+        	leftAuto = 0;
+    	}
+    	p2.style.left = leftAuto + 'px';
+	autoSlider();
+	}, 1000);
+}
+
+autoSlider();
