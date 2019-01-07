cca898afa18213c1ab9ff38b7ebf6291ca884be5
RomanLepehin <46236296+RomanLepehin@users.noreply.github.com>
2019-01-07 12:46:10 +0700

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,5 +1,24 @@
 document.getElementById('btn').onclick = sliderLeft;
+document.getElementById('btn-r').onclick = sliderRight;
+
 var left = 0;
+var right = 0;
 function sliderLeft() {
-    document.getElementById
+    var p;
+    p = document.getElementById('polosa');
+    left = left - 256;
+    if (left < -768) {
+        left = 0;
+    }
+    p.style.left = left + 'px';
+}
+
+function sliderRight() {
+   var p;
+    p = document.getElementById('polosa');
+    right = right + 256;
+//    if (left < 768) {
+//        left = 0;
+//    }
+    p.style.left = left - right + 'px';
 }
